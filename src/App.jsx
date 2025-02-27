c21cf9776f3ede378553b2caed5b94fe35d502d8
Varenik575 <invisibleredhead5@gmail.com>
2025-02-27 22:30:06 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Options from './components/Options/Options'
+function App() {
+
+  return (
+    <>
+<div>
+  <Options/>
+  
+</div>
+     </>
+  )
+}
+
+export default App
---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,14 @@
+import styles from './Options.module.css';
+
+const Options = () => {
+return (
+    <div>
+        <h1 className={styles.header}>Sip Happens Café</h1>
+        <p className={styles.text}>
+        Please leave your feedback about our service by selecting one of the options below.
+        </p>
+    </div>
+)
+}
+
+export default Options;
