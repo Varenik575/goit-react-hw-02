877dca233084fa69f7f1f4087f9c4be9d521c4fe
Varenik575 <invisibleredhead5@gmail.com>
2025-04-02 18:40:42 +0300

    fixed formatting issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,74 @@
-import './App.css';
-import Description from './components/Description/Description';
-import Feedback from './components/Feedback/Feedback';
-import Options from './components/Options/Options';
-import Notification from './components/Notification/Notification';
-import { useState, useEffect} from 'react';
+import './App.css'
+import Description from './components/Description/Description'
+import Feedback from './components/Feedback/Feedback'
+import Options from './components/Options/Options'
+import Notification from './components/Notification/Notification'
+import { useState, useEffect } from 'react'
 
 function App() {
+    const [options, setOption] = useState(() => {
+        const savedFeedback = JSON.parse(
+            window.localStorage.getItem('saved-feedback')
+        )
 
-const [options, setOption] = useState(() => {
- 
-  const savedFeedback = JSON.parse(window.localStorage.getItem("saved-feedback"));
+        if (savedFeedback) {
+            return savedFeedback.options
+        } else
+            return {
+                good: 0,
+                neutral: 0,
+                bad: 0,
+            }
+    })
 
+    const updateFeedback = (feedbackType) => {
+        switch (feedbackType) {
+            case 'good':
+                setOption({ ...options, good: options.good + 1 })
+                break
+            case 'neutral':
+                setOption({ ...options, neutral: options.neutral + 1 })
+                break
+            case 'bad':
+                setOption({ ...options, bad: options.bad + 1 })
+                break
+            case 'reset':
+                setOption({ ...options, good: 0, neutral: 0, bad: 0 })
+                break
+        }
+    }
 
-if((savedFeedback)) 
-{
-return savedFeedback.options;
-} else return ({
-  good: 0,
-  neutral: 0,
-  bad: 0
-})})
+    const totalFeedback = options.good + options.neutral + options.bad
+    const positiveFeedback = Math.round((options.good / totalFeedback) * 100)
 
-const updateFeedback = (feedbackType) => {
-  switch(feedbackType){
-    case('good'):
-    setOption({...options, good: options.good + 1});
-    break;
-    case('neutral'):
-    setOption({...options, neutral: options.neutral + 1});
-    break;
-    case('bad'):
-    setOption({...options, bad: options.bad + 1});
-    break;
-    case('reset'):
-    setOption({...options, good: 0, neutral:0, bad: 0 });
-    break;
-  }
- }
+    useEffect(() => {
+        window.localStorage.setItem(
+            'saved-feedback',
+            JSON.stringify({ options })
+        )
+    }, [options])
 
-const totalFeedback = options.good + options.neutral + options.bad;
-const positiveFeedback = Math.round((options.good / totalFeedback) * 100);
+    return (
+        <>
+            <div>
+                <Description />
 
-useEffect(() => {
-  window.localStorage.setItem("saved-feedback", JSON.stringify({options}))}, [options]);
+                <Options onClickFunc={updateFeedback} total={totalFeedback} />
 
-  return (
-    <>
-<div>
-<Description/>
+                {totalFeedback ? (
+                    <Feedback
+                        good={options.good}
+                        neutral={options.neutral}
+                        bad={options.bad}
+                        total={totalFeedback}
+                        positive={positiveFeedback}
+                    />
+                ) : (
+                    <Notification />
+                )}
+            </div>
+        </>
+    )
+}
 
- <Options 
- onClickFunc={updateFeedback}
- total={totalFeedback}/>
-
-{totalFeedback ? <Feedback
-good={options.good}
-neutral={options.neutral}
-bad={options.bad}
-total={totalFeedback}
-positive={positiveFeedback}
-/> : <Notification/>}
-
-</div>
-     </>
-  )
-};
-
-export default App;
+export default App
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,11 +1,9 @@
-import styles from './Options.module.css';
+import styles from './Options.module.css'
 
-
-const Options = ({onClickFunc, total}) => {
-
+const Options = ({ onClickFunc, total }) => {
     const renderReset = (total) => {
-        if(total){
-            return (<button onClick={() => onClickFunc('reset')}>Reset</button>);
+        if (total) {
+            return <button onClick={() => onClickFunc('reset')}>Reset</button>
         }
     }
 
@@ -15,10 +13,10 @@ const Options = ({onClickFunc, total}) => {
             <button onClick={() => onClickFunc('neutral')}>Neutral</button>
             <button onClick={() => onClickFunc('bad')}>Bad</button>
             {renderReset(total)}
-            
-       {/*    <button>Reset</button>  */}
+
+            {/*    <button>Reset</button>  */}
         </div>
     )
 }
 
-export default Options;
+export default Options
